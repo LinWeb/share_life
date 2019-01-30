d1a74fbd794515dc7b8bacef7faa47b17347cb85
林晓生 <798738148@qq.com>
2019-01-30 09:56:03 +0800

    上传文件夹如果不存在自动创建

--- config/index.js
+++ config/index.js
@@ -1,5 +1,22 @@
 let path = require('path')
+let fs = require('fs')
 const ROOT_PATH = path.join(__dirname, '../')
+const DYNAMIC_IMGS_UPLOAD_URL = '/upload/dynamic_imgs/'
+const HEAD_IMGS_UPLOAD_URL = '/upload/head_imgs/'
+let upload_path = path.join(ROOT_PATH, '/public/upload/')
+let dynamic_imgs_path = path.join(ROOT_PATH, '/public/upload/dynamic_imgs/')
+let head_imgs_path = path.join(ROOT_PATH, '/public/upload/head_imgs/')
+
+// 如果不存在此文件夹创建文件夹
+if (!fs.existsSync(upload_path)) {
+    fs.mkdirSync(upload_path) // 上传文件夹
+}
+if (!fs.existsSync(dynamic_imgs_path)) {
+    fs.mkdirSync(dynamic_imgs_path)  // 动态图片上传文件夹
+}
+if (!fs.existsSync(head_imgs_path)) {
+    fs.mkdirSync(head_imgs_path) // 头像图片上传文件夹
+}
 
 module.exports = {
     MAIN_DB_URL: 'mongodb://localhost:27017/share_life',
@@ -8,8 +25,8 @@ module.exports = {
     USER_DEFAULT_SIGN: '这个人很懒，个人签名都不写~',
     PUBLIC: 'public',
     DEFAULT_HEAD_URL: '/images/default_head.png',
-    DYNAMIC_IMGS_UPLOAD_URL: '/upload/dynamic_imgs/',
-    HEAD_IMGS_UPLOAD_URL: '/upload/head_imgs/',
+    DYNAMIC_IMGS_UPLOAD_URL,
+    HEAD_IMGS_UPLOAD_URL,
     RES_ERROR(err, res) {
         res.send({ status: 0, msg: err.message })
     },
