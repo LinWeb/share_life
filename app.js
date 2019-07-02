fd27b9cef5082b1e70da84382943b1da737a37be
lxs <798738148@qq.com>
2019-07-02 21:08:44 +0800

    去除跨域限制

--- app.js
+++ app.js
@@ -23,14 +23,15 @@ app.set('view engine', 'pug');
 
 // 设置头部
 app.use(function (req, res, next) {
-  let origin = req.headers.origin;
-  let allow_origins = [
-    'http://localhost:8000',
-    'http://39.105.228.74:8080',
-  ]
-  if (allow_origins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin)
-  }
+  // let origin = req.headers.origin;
+  // let allow_origins = [
+  //   'http://localhost:8000',
+  //   'http://39.105.228.74:8080',
+  // ]
+  // if (allow_origins.includes(origin)) {
+  //   res.setHeader('Access-Control-Allow-Origin', origin)
+  // }
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
   res.setHeader('Access-control-Allow-Methods', '*')
   res.setHeader('Access-control-Allow-Headers', 'X-Requested-With,content-type')
   res.setHeader('Access-control-Allow-Credentials', true)
